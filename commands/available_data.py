4b9e31d34e9453bdd01df0cfc5946697fb5934fa
Darren Rahnemoon <darrenrahnemoon@gmail.com>
2023-01-22 20:03:33 -0500

    feature: histogram for available data

--- commands/available_data.py
+++ commands/available_data.py
@@ -1,3 +1,5 @@
+import numpy
+import pandas
 from argparse import ArgumentParser, BooleanOptionalAction, Namespace
 
 from core.repository import Repository
@@ -6,24 +8,40 @@ from .utils.chart import add_chart_arguments, get_chart_filter
 
 def config(parser: ArgumentParser):
 	parser.add_argument('repository', type = RepresentationSerializer(Repository).deserialize)
-	parser.add_argument('--timestamps', action = BooleanOptionalAction)
 	parser.add_argument('--gap-percentage', action = BooleanOptionalAction)
+	parser.add_argument('--histogram', action = BooleanOptionalAction)
 	add_chart_arguments(parser, nargs = '*')
+
+def print_chart(chart):
+	print(
+		type(chart).__name__,
+		*[ getattr(chart, key) for key in chart.query_fields ],
+		chart.from_timestamp,
+		chart.to_timestamp,
+		getattr(chart, 'gap_percentage', None),
+		sep='\t'
+	)
 
 def handler(args: Namespace):
 	repository = args.repository()
 	charts = repository.get_available_charts(
 		filter = get_chart_filter(args),
-		include_timestamps = args.timestamps or args.gap_percentage
+		include_timestamps = True
 	)
-	for chart in charts:
-		gap_percentage = repository.get_gap_percentage(chart) if args.gap_percentage and hasattr(chart, 'interval') else None
+	charts = list(charts)
 
-		print(
-			type(chart).__name__,
-			*[ getattr(chart, key) for key in chart.query_fields ],
-			chart.from_timestamp,
-			chart.to_timestamp,
-			f'{gap_percentage * 100}%',
-			sep='\t'
-		)
+	if args.gap_percentage:
+		for chart in charts:
+			chart.gap_percentage = repository.get_gap_percentage(chart)
+
+	if args.histogram:
+		_, edges = numpy.histogram([ chart.from_timestamp.to_pydatetime().timestamp() for chart in charts ], bins = 'auto')
+		edges = [ pandas.to_datetime(edge, unit = 's', utc = True) for edge in edges ]
+		for index in range(1, len(edges)):
+			print(f'\n\n{index}: {edges[index - 1]} - {edges[index]}')
+			for chart in charts:
+				if chart.from_timestamp > edges[index - 1] and chart.from_timestamp < edges[index]:
+					print_chart(chart)
+	else:
+		for chart in charts:
+			print_chart(chart)
