539cde55f0e8183a2e05e3d21473c4cea045324a
Darren Rahnemoon <darrenrahnemoon@gmail.com>
2023-09-22 13:18:15 -0400

    chore: more cleanup

--- core/chart/serializers.py
+++ core/chart/serializers.py
@@ -24,64 +24,66 @@ class ChartRecordsSerializer(Serializer):
 	) -> pandas.DataFrame:
 		# Note: this serializer is the only serializer that will mutate the records if given a dataframe
 
-		# Convert records to dataframe
-		if hasattr(value, '__iter__'):
-			if not isinstance(value, (pandas.DataFrame, pandas.Series)):
+		# Convert records to dataframe if it's already not
+		dataframe = value
+		if not isinstance(value, (pandas.DataFrame, pandas.Series)):
+			if hasattr(value, '__iter__'):
 				if type(value) != list:
 					value = list(value)
 					# pandas.DataFrame constructor doesn't directly accept iterators
-					value = pandas.DataFrame.from_records(value)
+					dataframe = pandas.DataFrame.from_records(value)
 
 		# Skip if not a dataframe
-		if type(value) != pandas.DataFrame:
-			if value != None: # None is expected occasionally
-				logger.warn(f'Unrecognized value passed to {type(self).__name__}.to_dataframe:\n{value}')
-			return value
+		if type(dataframe) != pandas.DataFrame:
+			if dataframe != None: # None is expected occasionally
+				logger.warn(f'Unrecognized value passed to {type(self).__name__}.to_dataframe:\n{dataframe}')
+			return dataframe
 
 		# Empty dataframes might not have the right columns, so add them
-		if len(value) == 0 and type(value.columns) != pandas.MultiIndex: # NOTE: `select` not supported for MultiIndex columns
-			value['timestamp'] = None
+		if len(dataframe) == 0 and type(dataframe.columns) != pandas.MultiIndex: # NOTE: `select` not supported for MultiIndex columns
+			dataframe['timestamp'] = None
 			if select and len(select):
-				value[select] = None
+				dataframe[select] = None
 
 		# Ensure timestamp is index
-		if type(value.index) != pandas.DatetimeIndex:
-			value.index = pandas.DatetimeIndex(value['timestamp'], name = 'timestamp')
-			value = value.drop(columns = [ 'timestamp' ])
+		if type(dataframe.index) != pandas.DatetimeIndex:
+			dataframe.index = pandas.DatetimeIndex(dataframe['timestamp'], name = 'timestamp')
+			dataframe = dataframe.drop(columns = [ 'timestamp' ])
 
 		# Make sure the dataframe is ascending
-		if len(value.index) > 1 and value.index[0] > value.index[-1]:
-			value = value.sort_index(ascending = True)
+		if len(dataframe.index) > 1 and dataframe.index[0] > dataframe.index[-1]:
+			dataframe = dataframe.sort_index(ascending = True)
 
 		# Ensure timestamp is timezone aware since different brokers have different time zones
-		if not value.index.tz:
-			value.index = value.index.tz_localize(tz = tz)
+		if not dataframe.index.tz:
+			dataframe.index = dataframe.index.tz_localize(tz = tz)
 
 		# Unflatten columns if columns are flattened
-		if is_any_of(value.columns, lambda column: MULTI_INDEX_COLUMN_SEPARATOR in column and type(column) == str):
-			value.columns = pandas.MultiIndex.from_tuples([ column.split(MULTI_INDEX_COLUMN_SEPARATOR) for column in value.columns ])
+		if is_any_of(dataframe.columns, lambda column: MULTI_INDEX_COLUMN_SEPARATOR in column and type(column) == str):
+			dataframe.columns = pandas.MultiIndex.from_tuples([ column.split(MULTI_INDEX_COLUMN_SEPARATOR) for column in dataframe.columns ])
 
-		# set column types from Chart.Query schema
+		# set column types from Chart.Record schema
 		for field in fields(self.chart_class.Record):
-			if not field.name in value.columns:
+			if not field.name in dataframe.columns:
 				continue
 
-			if value.dtypes[field.name] == field.type:
+			if dataframe.dtypes[field.name] == field.type:
 				continue
 
-			value[field.name] = value[field.name].astype(field.type)
+			dataframe[field.name] = dataframe[field.name].astype(field.type)
 
 		# Add the wrapping column based on the chart specified
-		if type(value.columns) != pandas.MultiIndex:
+		if type(dataframe.columns) != pandas.MultiIndex:
 			if select and len(select):
-				value = value[[ key for key in value.columns if key in select ]]
+				dataframe = dataframe[[ key for key in dataframe.columns if key in select ]]
 
 			if name:
-				value.columns = pandas.MultiIndex.from_tuples(
-					[ (name, column) for column in value.columns ],
+				dataframe.columns = pandas.MultiIndex.from_tuples(
+					[ (name, column) for column in dataframe.columns ],
 					names=[ 'chart', 'field' ]
 				)
-		return value
+
+		return dataframe
 
 	def to_records(self, dataframe: pandas.DataFrame):
 		if type(dataframe.columns) == pandas.MultiIndex:
